b6b8157b16fe37cada18f6e485a0578e987bed5f
Angelmário Santana <angelmario.santana@live.com>
2015-01-13 17:41:02 -0300

    Movido operações do main para métodos em uma classe específica.

--- PhytonAlgoritmoGenetico/classes/Gene.py
+++ PhytonAlgoritmoGenetico/classes/Gene.py
@@ -2,7 +2,7 @@ from random import Random
 rand = Random()
 class Gene:
     def __init__(self):
-      self.informacao = rand.randint(1,9);
+      self.informacao =  rand.randint(1,9);
 
    
     
---
+++ PhytonAlgoritmoGenetico/funcoes/Genetico.py
@@ -0,0 +1,25 @@
+from Gene import Gene
+from Cromossomo import Cromossomo
+from Candidato import Candidato
+class Genetico:
+
+    @staticmethod    
+    def gerarCandidatos():  
+        candidatos = Candidato(list())
+        for x in range(0, 4):
+            cromossomo = Cromossomo(list())
+            for g in range(0, 4):
+                gene = Gene()   
+                cromossomo.listaGene.append(gene)
+            candidatos.listaCandidatos.append(cromossomo)
+        return  candidatos
+
+    @staticmethod    
+    def listarCandidatos():  
+        try:
+            for candidato in Genetico.gerarCandidatos().listaCandidatos:    
+                for gene in candidato.listaGene:            
+                    print ("%d" % gene.informacao, end="", flush=True)
+                print("\n")
+        except Exception as ex:
+            print("Ocorreu o erro: {0} ".format(ex.args))
